3225316edf6945121f4b5a153532c30ffed0e96a
Prajita Tandukar <167902313+prajitat@users.noreply.github.com>
2025-02-18 15:56:30 -0500

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,5 +1,15 @@
 import qrcode
+from datetime import datetime
 
+def generate_qr():
+    #Get user input for the QR code content
+    data = input("Enter the data for the QR code (URL/Text): ").strip()
+
+if not data:
+    print("Error: No data was provided. Please enter valid text or a URL.")
+    return
+
+# Create a QR code instance
 qr = qrcode.QRCode(
     version = 1,
     error_correction = qrcode.constants.ERROR_CORRECT_L,
@@ -7,9 +17,13 @@ qr = qrcode.QRCode(
     border = 4
 )
 
+# Add data to the QR code
+
 data = "https://prajita.vercel.app"
 qr.add_data(data)
 qr.make(fit = True)
+
+
 
 img = qr.make_image(fill='black', back_color = 'white')
 img.save("newqr.png")
